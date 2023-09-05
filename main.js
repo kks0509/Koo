978c07d444723e6681bcf33272ea41e54aae7f15
unknown <ks509koo@gmail.com>
2023-09-05 13:00:27 +0900

    1st

---
+++ main.js
@@ -0,0 +1,35 @@
+document.getElementById('time').innerHTML = new Date().toLocaleString();
+
+function pink() {
+  document.body.style.backgroundColor = 'pink';
+}
+
+function lightblue() {
+  document.body.style.backgroundColor = 'lightblue';
+}
+
+function yellow() {
+  document.body.style.backgroundColor = 'yellow';
+}
+
+function reset() {
+  document.body.style.backgroundColor = 'linen';
+}
+
+function showhtml() {
+  document.getElementById('fig').src = 'img/html.jpg';
+  document.getElementById('desc').innerHTML =
+    '<b>HTML</b>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다.';
+}
+
+function showcss() {
+  document.getElementById('fig').src = 'img/html5.jpg';
+  document.getElementById('desc').innerHTML =
+    '<b>CSS</b>는 HTML 문서의 스타일을 지정하는 언어이다.';
+}
+
+function showcss() {
+  document.getElementById('fig').src = 'img/html5.jpg';
+  document.getElementById('desc').innerHTML =
+    '<b>CSS</b>는 HTML 문서의 스타일을 지정하는 언어이다.';
+}
